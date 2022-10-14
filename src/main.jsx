55941dc1886ed527e79e5119253627ca9cfd4b78
AbdulRehmanArshad <13pfye2046@gmail.com>
2022-10-14 23:40:38 +0500

    Add Formik and Yup Validation

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import "rsuite/dist/rsuite.min.css";
 import "@fontsource/comfortaa";
 import "font-awesome/css/font-awesome.css";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
